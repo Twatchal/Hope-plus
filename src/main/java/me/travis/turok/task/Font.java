55392849241f7dc687e7b5ead116f02b383ad0b4
Twatchal <84936618+Twatchal@users.noreply.github.com>
2022-02-28 19:45:45 -0600

    Add files via upload

---
+++ src/main/java/me/travis/turok/task/Font.java
@@ -0,0 +1,35 @@
+package me.travis.turok.task;
+
+public class Font
+{
+    public static String smoth(final String base) {
+        String new_base = base;
+        new_base = new_base.replace("a", "a");
+        new_base = new_base.replace("b", "b");
+        new_base = new_base.replace("c", "c");
+        new_base = new_base.replace("d", "d");
+        new_base = new_base.replace("e", "e");
+        new_base = new_base.replace("f", "f");
+        new_base = new_base.replace("g", "g");
+        new_base = new_base.replace("h", "h");
+        new_base = new_base.replace("i", "i");
+        new_base = new_base.replace("j", "j");
+        new_base = new_base.replace("k", "k");
+        new_base = new_base.replace("l", "l");
+        new_base = new_base.replace("m", "m");
+        new_base = new_base.replace("n", "n");
+        new_base = new_base.replace("o", "o");
+        new_base = new_base.replace("p", "p");
+        new_base = new_base.replace("q", "q");
+        new_base = new_base.replace("r", "r");
+        new_base = new_base.replace("s", "s");
+        new_base = new_base.replace("t", "t");
+        new_base = new_base.replace("u", "u");
+        new_base = new_base.replace("v", "v");
+        new_base = new_base.replace("w", "w");
+        new_base = new_base.replace("x", "x");
+        new_base = new_base.replace("y", "y");
+        new_base = new_base.replace("z", "z");
+        return new_base;
+    }
+}
