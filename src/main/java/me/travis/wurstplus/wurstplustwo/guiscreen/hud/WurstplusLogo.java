625bb3b646d8e1fccf4d5e608e7c388b29301273
Twatchal <84936618+Twatchal@users.noreply.github.com>
2022-03-31 22:06:43 -0600

    Add files via upload

--- src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusLogo.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusLogo.java
@@ -1,23 +1,22 @@
 package me.travis.wurstplus.wurstplustwo.guiscreen.hud;
 
-import me.travis.wurstplus.wurstplustwo.guiscreen.render.pinnables.*;
-import net.minecraft.util.*;
-import org.lwjgl.opengl.*;
-import me.travis.wurstplus.wurstplustwo.util.*;
+import me.travis.wurstplus.wurstplustwo.guiscreen.render.pinnables.WurstplusPinnable;
+import me.travis.wurstplus.wurstplustwo.util.WurstplusTextureHelper;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
-public class WurstplusLogo extends WurstplusPinnable
-{
-    ResourceLocation r;
-    
+public class WurstplusLogo
+extends WurstplusPinnable {
+    ResourceLocation r = new ResourceLocation("custom/wurst.png");
+
     public WurstplusLogo() {
         super("Logo", "Logo", 1.0f, 0, 0);
-        this.r = new ResourceLocation("custom/wurst.png");
     }
-    
+
     public void render() {
         GL11.glPushMatrix();
-        GL11.glTranslatef((float)this.get_x(), (float)this.get_y(), 0.0f);
-        WurstplusTextureHelper.drawTexture(this.r, (float)this.get_x(), (float)this.get_y(), 100.0f, 25.0f);
+        GL11.glTranslatef((float)this.get_x(), (float)this.get_y(), (float)0.0f);
+        WurstplusTextureHelper.drawTexture((ResourceLocation)this.r, (float)this.get_x(), (float)this.get_y(), (float)100.0f, (float)25.0f);
         GL11.glPopMatrix();
         this.set_width(100);
         this.set_height(25);
--- src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusWatermark.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusWatermark.java
@@ -1,20 +1,22 @@
 package me.travis.wurstplus.wurstplustwo.guiscreen.hud;
 
-import me.travis.wurstplus.wurstplustwo.guiscreen.render.pinnables.*;
-import me.travis.wurstplus.*;
+import com.mojang.realmsclient.gui.ChatFormatting;
+import me.travis.wurstplus.Wurstplus;
+import me.travis.wurstplus.wurstplustwo.guiscreen.render.pinnables.WurstplusPinnable;
+import me.travis.wurstplus.wurstplustwo.guiscreen.settings.WurstplusSetting;
 
-public class WurstplusWatermark extends WurstplusPinnable
-{
+public class WurstplusWatermark
+extends WurstplusPinnable {
     public WurstplusWatermark() {
         super("Watermark", "Watermark", 1.0f, 0, 0);
     }
-    
+
     public void render() {
-        final int nl_r = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorR").get_value(1);
-        final int nl_g = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorG").get_value(1);
-        final int nl_b = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(1);
-        final int nl_a = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorA").get_value(1);
-        final String line = "§9Hope+" + Wurstplus.g + " v" + Wurstplus.get_version();
+        int nl_r = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorR").get_value(1);
+        int nl_g = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorG").get_value(1);
+        int nl_b = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(1);
+        int nl_a = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorA").get_value(1);
+        String line = "\u00a79Hope+" + (Object)Wurstplus.g + " v" + Wurstplus.get_version();
         this.create_line(line, this.docking(1, line), 2, nl_r, nl_g, nl_b, nl_a);
         this.set_width(this.get(line, "width") + 2);
         this.set_height(this.get(line, "height") + 2);
